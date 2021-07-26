109ca9e10644eba34e330f21ab4277b60a3924b0
Samuel <silvasmlsss.02@gmail.com>
2021-07-25 22:05:57 -0300

    Comitando ...

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
-
+//
 function criarColecao(){
 
     const MongoClient = require("mongodb").MongoClient
